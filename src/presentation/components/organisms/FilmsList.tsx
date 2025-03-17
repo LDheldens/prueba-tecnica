5c512c03a32a1d7882a7ec6023483975b97f1c3a
LDheldens <daniavici7895123@gmail.com>
2025-03-16 21:01:34 -0500

    feat(components): implement FilmList component for films collection

---
+++ src/presentation/components/organisms/FilmsList.tsx
@@ -0,0 +1,28 @@
+// components/organisms/PlanetList.tsx
+import React from 'react';
+import { FlatList } from 'react-native';
+import PlanetCard from '../molecules/PlanetCard';
+import { Pelicula, Planet } from '../../../types/types';
+import FilmCard from '../molecules/FilmCard';
+
+interface Props {
+  peliculas?: Pelicula[];
+  onPress: (url: string) => void;
+}
+
+const PlanetList = ({ peliculas = [], onPress }: Props) => {
+  return (
+    <FlatList
+      data={peliculas}
+      keyExtractor={(item) => item.url}
+      renderItem={({ item }) => (
+        <FilmCard
+            film={item}
+            onPress={()=>onPress(item.url)}
+        />
+      )}
+    />
+  );
+};
+
+export default PlanetList;
