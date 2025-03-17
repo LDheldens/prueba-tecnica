029f869070dc73e013ea547acecf91cf5c6b2941
LDheldens <daniavici7895123@gmail.com>
2025-03-16 20:23:09 -0500

    feat: add DetailRow component for displaying key-value pairs

---
+++ src/presentation/components/atoms/DetailRow.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import { colors, golbalStyles } from "../../../theme/theme";
+
+interface Props {
+    label:string,
+    value?:string
+}
+
+const DetailRow = ({ label, value='' }:Props) => {
+    return (
+      <View style={styles.detailRow}>
+        <Text style={[styles.texts, { color: '#fff' }]}>{label}: </Text>
+        <Text style={[styles.texts,{ color: colors.secondary}]}>{value}</Text>
+      </View>
+    );
+};
+
+export default DetailRow
+
+const styles = StyleSheet.create({
+    detailRow: {
+        flexDirection: 'row',
+        marginBottom: 8,
+    },
+    texts:{
+        ...golbalStyles.title2,
+        textAlign:'left'
+      }
+})
