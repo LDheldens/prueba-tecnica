401d9bb2f36792345138e0e4c0c82dc37cdb5a5d
LDheldens <daniavici7895123@gmail.com>
2025-03-17 19:59:40 -0500

    refactor(*)minimal changes

--- src/presentation/components/pages/tabs/Tab2Screen.tsx
+++ src/presentation/components/pages/tabs/Tab2Screen.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { golbalStyles } from '../../../../theme/theme'
 
 const Tab2Screen = () => {
   return (
     <View>
-      <Text>tab2Screen</Text>
+      <Text style={golbalStyles.title1}>Favorites screen in development</Text>
     </View>
   )
 }
